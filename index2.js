48b54db25e29856d448204f5eba7f663182b7a16
Mark Andrus Roberts <markandrusroberts@gmail.com>
2013-04-16 17:23:40 -0500

    Update

---
+++ index2.js
@@ -0,0 +1,8 @@
+module.exports = {
+  constructors: require('./lib2/constructors.js'),
+  Semigroup: require('./lib2/Semigroup.js'),
+  Monoid: require('./lib2/Monoid.js'),
+  Functor: require('./lib2/Functor.js'),
+  Applicative: require('./lib2/Applicative.js'),
+  Monad: require('./lib2/Monad.js')
+};
